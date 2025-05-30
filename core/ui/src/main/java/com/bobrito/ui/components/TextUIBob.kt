8ba0028b309f1a4e5ad44b5195c295b213731a13
algididntwakeup <algisosmedacc@gmail.com>
2025-05-30 11:17:57 +0700

    adding text components n testing

---
+++ core/ui/src/main/java/com/bobrito/ui/components/TextUIBob.kt
@@ -0,0 +1,51 @@
+package com.bobrito.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun BobTextHeader (
+    text: String = "Welcome to GAIIA",
+    color: Color = Color.Black,
+    modifier: Modifier = Modifier.padding(16.dp)
+) {
+    Text(
+           text =text,
+           modifier = modifier,
+           style = TextStyle(
+           fontSize = 20.sp,
+           fontWeight = FontWeight.Bold,
+           lineHeight = 24.sp,
+           color = Color.Black,
+           textAlign = TextAlign.Left
+       ),
+        overflow = TextOverflow.Ellipsis,
+        color = color,
+        maxLines = 1
+    )
+}
+
+@Composable
+fun BobTextFooter () {
+    Text(text = "Lupa Password?")
+}
+
+@Preview(device = Devices.PIXEL, showSystemUi = true)
+@Composable
+fun BobTextHeaderPreview () {
+    BobTextHeader(
+        text = "kembali lgi bersama sy jeremi teti"
+    )
+}
