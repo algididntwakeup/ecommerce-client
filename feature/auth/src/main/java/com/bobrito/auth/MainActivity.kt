5a4fe8b837120b5f018b148dfa7c785af7001c46
algididntwakeup <algisosmedacc@gmail.com>
2025-06-03 09:57:52 +0700

    adding navigation compose library

--- feature/auth/src/main/java/com/bobrito/auth/MainActivity.kt
+++ feature/auth/src/main/java/com/bobrito/auth/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.bobrito.auth.ui.signin.SigninScreen
+import com.bobrito.auth.ui.signup.SignupScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +23,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             com.bobrito.ui.theme.ShoecommerceappTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "El Bobrito Bondito",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-                SigninScreen()
+                val navController = rememberNavController()
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "signin",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("signin") {
+                            SigninScreen()
+                        }
+                        composable("signup") {
+                            SignupScreen()
+                        }
+                    }
+                }
             }
         }
     }
